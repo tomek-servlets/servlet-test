efee4565fdc667944b49156b1cdd8abef9366195
test <test@MacBook-Air-Tomasz.local>
2017-05-15 00:49:25 +0200

    next commit

--- src/main/java/com/example/test/Test.java
+++ src/main/java/com/example/test/Test.java
@@ -25,7 +25,7 @@ public class Test extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Welcome to the servlet");
+		response.getWriter().append("Welcome to the servlet. We are on OpenShift");
 	}
 
 	/**
