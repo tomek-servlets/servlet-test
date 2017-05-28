8c11ec74642736c52e2c4404e70d6f80f5eef7a9
ironbone <iron.bone@wp.pl>
2017-05-28 23:36:57 +0200

    first commit Patryk

--- src/com/patryk/adaramelech/DBInfo.java
+++ src/com/patryk/adaramelech/DBInfo.java
@@ -1,15 +1,15 @@
 package com.patryk.adaramelech;
 
 public class DBInfo {
-	static String host = "localhost"; // In most cases you will use
+	static String host = "mysql"; // In most cases you will use
 													// localhost
-	static String DBname = "shopcomputer";
+	static String DBname = "lllc";
 	static int port = 3306; // This is for MySQL
 	static String mySQLdbURL = "jdbc:mysql://" + host + ":" + port + "/"
 			+ DBname;
 
 	static String user = "root";
-	static String password = "";
+	static String password = "root";
 	
 	static String driver = "com.mysql.jdbc.Driver";
 
